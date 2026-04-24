f09d1e8a78f8522015ab1d236f1acd22d9c661ba
Liton <liton01766@gmail.com>
2026-04-25 02:16:14 +0600

    Create Navbar

--- src/components/layouts/MainLayouts.jsx
+++ src/components/layouts/MainLayouts.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import TopBar from '../header/TopBar'
 import { Outlet } from 'react-router'
 import MainHeader from '../header/MainHeader'
+import Navbar from '../header/Navbar'
 
 const MainLayouts = () => {
   return (
     <>
       <TopBar />
       <MainHeader />
+      <Navbar />
       <Outlet />
     </>
   )
