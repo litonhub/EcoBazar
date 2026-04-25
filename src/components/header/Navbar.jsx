8158afc973ac6395015afd9e556f7bd6d58aab92
Liton <liton01766@gmail.com>
2026-04-25 13:48:09 +0600

    Create Header
    Create common dropdown, custom hook: OutsideClick and many customization.

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,114 +1,100 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState } from 'react'
 import Container from '../layouts/Container'
-import { FaAngleDown } from "react-icons/fa6";
+import { FaAngleDown, FaBars, FaChevronDown, FaPlus } from "react-icons/fa";
 import { TbPhoneCall } from "react-icons/tb";
-import { FaBars, FaChevronDown, FaPlus } from "react-icons/fa";
 import { FaAppleAlt, FaCarrot, FaFish, FaDrumstickBite, FaWineBottle, FaIceCream, FaBreadSlice, FaCheese, FaUtensils } from "react-icons/fa";
+import Dropdown from "../common/Dropdown";
 
 const Navbar = () => {
 
-    const [open, setOpen] = useState(false);
-    const [active, setActive] = useState("Vegetables");
-    const dropdownRef = useRef(null);
+  const [active, setActive] = useState("Vegetables");
 
-    // 👉 outside click close
-    useEffect(() => {
-        const handleClickOutside = (e) => {
-            if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
-                setOpen(false);
-            }
-        };
+  const menuItems = [
+    { name: "Home", dropdown: true },
+    { name: "Shop", dropdown: true },
+    { name: "Pages", dropdown: true },
+    { name: "Blog", dropdown: true },
+    { name: "About Us", dropdown: false },
+    { name: "Contact Us", dropdown: false },
+  ];
 
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => document.removeEventListener("mousedown", handleClickOutside);
-    }, []);
+  const categories = [
+    { name: "Fresh Fruit", icon: <FaAppleAlt /> },
+    { name: "Vegetables", icon: <FaCarrot /> },
+    { name: "River Fish", icon: <FaFish /> },
+    { name: "Chicken & Meat", icon: <FaDrumstickBite /> },
+    { name: "Drink & Water", icon: <FaWineBottle /> },
+    { name: "Yogurt & Ice Cream", icon: <FaIceCream /> },
+    { name: "Cake & Bread", icon: <FaBreadSlice /> },
+    { name: "Butter & Cream", icon: <FaCheese /> },
+    { name: "Cooking", icon: <FaUtensils /> },
+  ];
 
-    const menuItems = [
-        { name: "Home", dropdown: true },
-        { name: "Shop", dropdown: true },
-        { name: "Pages", dropdown: true },
-        { name: "Blog", dropdown: true },
-        { name: "About Us", dropdown: false },
-        { name: "Contact Us", dropdown: false },
-    ];
-
-    const categories = [
-        { name: "Fresh Fruit", icon: <FaAppleAlt /> },
-        { name: "Vegetables", icon: <FaCarrot /> },
-        { name: "River Fish", icon: <FaFish /> },
-        { name: "Chicken & Meat", icon: <FaDrumstickBite /> },
-        { name: "Drink & Water", icon: <FaWineBottle /> },
-        { name: "Yogurt & Ice Cream", icon: <FaIceCream /> },
-        { name: "Cake & Bread", icon: <FaBreadSlice /> },
-        { name: "Butter & Cream", icon: <FaCheese /> },
-        { name: "Cooking", icon: <FaUtensils /> },
-    ];
-
-    return (
-        <div className='bg-logoc'>
-            <Container>
-                <div className='flex items-center gap-x-8 font-pop leading-[150%]'>
-                    <div ref={dropdownRef} className="relative w-78">
-                        <div
-                            onClick={() => setOpen(!open)}
-                            className="flex items-center justify-between bg-subb text-white cursor-pointer"
-                        >
-                            <div className="flex items-center">
-                                <div className="bg-primary p-4">
-                                    <FaBars className='size-8' />
-                                </div>
-                                <h4 className="px-4 py-5 font-medium text-[16px]">All Categories</h4>
-                            </div>
-                            <FaChevronDown
-                                className={`mr-4 size-5 transition-transform duration-300 ${open ? "rotate-180" : ""}`}
-                            />
-                        </div>
-                        <div
-                            className={`absolute left-0 top-full w-full bg-white shadow-lg z-50
-                            transition-all duration-300 origin-top
-                            ${open ? "scale-y-100 opacity-100" : "scale-y-0 opacity-0"}`}
-                        >
-                            <ul className="border border-solid border-brdr">
-                                {categories.map((item, index) => (
-                                    <li
-                                        key={index}
-                                        onClick={() => setActive(item.name)}
-                                        className="flex items-center gap-3 px-5 py-4 text-sm font-normal cursor-pointer transition-all duration-300 text-logoc hover:bg-primary hover:text-white"
-                                    >
-                                        <span className="text-lg">{item.icon}</span>
-                                        {item.name}
-                                    </li>
-                                ))}
-                                <li className="flex items-center gap-3 px-5 py-4 text-sm font-normal text-logoc border-t border-solid border-brdr cursor-pointer hover:bg-primary hover:text-white transition">
-                                    <FaPlus />
-                                    View all Category
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="flex items-center justify-between w-full">
-                        <div>
-                            <ul className="flex text-[#999999] text-sm font-medium gap-x-8">
-                                {menuItems.map((item, index) => (
-                                    <li
-                                        key={index}
-                                        className="flex items-center gap-1 hover:text-white transition-all duration-300 ease-in-out cursor-pointer"
-                                    >
-                                        {item.name}
-                                        {item.dropdown && <FaAngleDown />}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                        <div className='flex items-center gap-x-2 text-white cursor-pointer'>
-                            <TbPhoneCall className='size-7' />
-                            <h4 className='text-sm font-medium'>(219) 555-0114</h4>
-                        </div>
-                    </div>
+  return (
+    <div className='bg-logoc'>
+      <Container>
+        <div className='flex items-center gap-x-8 font-pop leading-[150%]'>
+          <Dropdown
+            options={categories}
+            value={active}
+            onChange={(item) => setActive(item.name)}
+            className="w-78"
+            dropdownClass="w-full border border-brdr"
+            renderTrigger={(open) => (
+              <div className="flex items-center justify-between bg-subb text-white">
+                <div className="flex items-center">
+                  <div className="bg-primary p-4">
+                    <FaBars className="size-8" />
+                  </div>
+                  <h4 className="px-4 py-5 font-medium text-[16px]">
+                    All Categories
+                  </h4>
                 </div>
-            </Container>
+                <FaChevronDown
+                  className={`mr-4 size-5 transition duration-300 ${
+                    open ? "rotate-180" : ""
+                  }`}
+                />
+              </div>
+            )}
+            renderItem={(item) => (
+              <div className="flex items-center gap-3 px-5 py-4 text-sm cursor-pointer hover:bg-primary hover:text-white transition">
+                <span className="text-lg">{item.icon}</span>
+                {item.name}
+              </div>
+            )}
+
+            footer={(
+              <div className="flex items-center gap-3 px-5 py-4 text-sm cursor-pointer hover:bg-primary hover:text-white transition border-t border-brdr">
+                <FaPlus />
+                View all Category
+              </div>
+            )}
+          />
+
+          <div className="flex items-center justify-between w-full">
+            <div>
+              <ul className="flex text-[#999999] text-sm font-medium gap-x-8">
+                {menuItems.map((item, index) => (
+                  <li
+                    key={index}
+                    className="flex items-center gap-1 hover:text-white transition cursor-pointer"
+                  >
+                    {item.name}
+                    {item.dropdown && <FaAngleDown />}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className='flex items-center gap-x-2 text-white cursor-pointer'>
+              <TbPhoneCall className='size-7' />
+              <h4 className='text-sm font-medium'>(219) 555-0114</h4>
+            </div>
+          </div>
         </div>
-    )
+      </Container>
+    </div>
+  )
 }
 
-export default Navbar
+export default Navbar;
--- src/components/header/TopBar.jsx
+++ src/components/header/TopBar.jsx
@@ -1,27 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from '../layouts/Container'
 import { CiLocationOn } from "react-icons/ci";
-import { FaAngleDown } from "react-icons/fa6";
+import Dropdown from '../common/Dropdown';
 
 const TopBar = () => {
+
+  const [language, setLanguage] = useState("Eng");
+  const [currency, setCurrency] = useState("USD");
+
   return (
     <div className='border-b border-solid border-brdr'>
-     <Container>
-        <div className='flex justify-between font-pop font-normal text-sm text-gry leading-[130%] py-3'>
-            <div className='flex items-center gap-2'><CiLocationOn /> Store Location: Lincoln- 344, Illinois, Chicago, USA</div>
-            <div className='flex gap-x-10'>
-                <div className='flex gap-x-5'>
-                    <div className='flex items-center gap-x-1.5'>Eng <FaAngleDown /></div>
-                    <div className='flex items-center gap-x-1.5'>USD <FaAngleDown /></div>
-                </div>
-                <div className='relative after:w-px after:h-3.75 after:bg-brdr after:content-[] after:absolute after:top-0.5 after:-left-5'>
-                    Sign In / Sign Up
-                </div>
+      <Container>
+        <div className='flex justify-between items-center font-pop font-normal text-sm leading-[130%] text-gry py-3'>
+          <div className='flex items-center gap-2'>
+            <CiLocationOn />
+            Store Location: Lincoln- 344, Illinois, Chicago, USA
+          </div>
+          <div className='flex items-center gap-x-10'>
+            <div className='flex items-center gap-x-5'>
+
+              <Dropdown
+                options={["Eng", "Bng", "Fra"]}
+                value={language}
+                onChange={setLanguage}
+              />
+              <Dropdown
+                options={["USD", "BDT", "EUR"]}
+                value={currency}
+                onChange={setCurrency}
+              />
             </div>
+            <div className='relative after:w-px after:h-3.75 after:bg-brdr after:content-[] after:absolute after:top-0.5 after:-left-5 cursor-pointer'>
+              Sign In / Sign Up
+            </div>
+          </div>
         </div>
-     </Container>
+      </Container>
     </div>
   )
 }
 
-export default TopBar
+export default TopBar;
