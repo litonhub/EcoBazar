5ab0bc92470e9c39719df6a86bcb2a1319c1c9e5
Liton <liton01766@gmail.com>
2026-07-20 21:03:13 +0600

    Use Live url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ecobazar-api.onrender.com/api",
   withCredentials: true,
 });
 // https://ecobazar-api.onrender.com
@@ -57,7 +57,7 @@ api.interceptors.response.use(
 
       try {
         const res = await axios.post(
-          "http://localhost:5000/api/auth/refresh",
+          "https://ecobazar-api.onrender.com/api/auth/refresh",
           {},
           { withCredentials: true }
         );
